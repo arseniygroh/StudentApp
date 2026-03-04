923ae37c66d48c05a0d5e43e0e18b6056a413293
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-03-04 16:48:32 +0200

    separated updateStudent logic from RegistryManager to make RegistryManger to look simpler

--- src/main/java/ukma/model/utils/Menu.java
+++ src/main/java/ukma/model/utils/Menu.java
@@ -6,10 +6,7 @@ import ukma.model.enums.StudentStatus;
 import ukma.model.enums.StudyForm;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Menu {
     private Scanner scan = new Scanner(System.in);
@@ -46,7 +43,7 @@ public class Menu {
                 } else System.out.println("You don't have a right to do it");
 
             } else if (option == 3) {
-                if (authService.isManager()) manager.updateStudent();
+                if (authService.isManager()) handleUpdateStudent();
                 else System.out.println("You don't have a right to do it");
 
             } else if (option == 4) {
@@ -162,4 +159,122 @@ public class Menu {
         return chosen;
     }
 
+    public void handleUpdateStudent() {
+        boolean isRunning = true;
+        while (isRunning) {
+            int studentId = inputValidator.readInt("Enter an id of a student you want to find: ", 1, 100);
+            Optional<Student> optionalStudent = Optional.ofNullable(manager.getStudentById(studentId).get(0));
+            if (optionalStudent.isPresent()) {
+                Student studentToUpdate = optionalStudent.get();
+                while (true) {
+                    System.out.println("What would you like to update? " + "\n"
+                            + "0 - update another student" + "\n"
+                            + "1 - first name" + "\n"
+                            + "2 - last name" + "\n"
+                            + "3 - father name" + "\n"
+                            + "4 - course code" + "\n"
+                            + "5 - study form" + "\n"
+                            + "6 - student status" + "\n"
+                            + "7 - email address" + "\n"
+                            + "8 - phone number" + "\n"
+                            + "Q or q - quit" + "\n");
+
+                    String optionStr = inputValidator.readString("Choose option: ").trim();
+
+                    if (optionStr.equalsIgnoreCase("q")) {
+                        isRunning = false;
+                        break;
+                    }
+                    int option = -1;
+                    try {
+                        option = Integer.parseInt(optionStr);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please enter a number from 0 to 8 or 'q' to quit");
+                        continue;
+                    }
+
+                    if (option < 0 || option > 8) continue;
+
+                    if (option == 1) {
+                        String name = inputValidator.readString("Enter new name for the student");
+                        studentToUpdate.setFirstName(name);
+                    } else if (option == 2) {
+                        String lastName = inputValidator.readString("Enter new last name for the student");
+                        studentToUpdate.setLastName(lastName);
+                    } else if (option == 3) {
+                        String fatherName = inputValidator.readString("Enter new fathers name for the student");
+                        studentToUpdate.setFatherName(fatherName);
+                    } else if (option == 4) {
+                        String courseCode = inputValidator.readString("Enter new course code for the student");
+                        studentToUpdate.setCourseCode(courseCode);
+                    } else if (option == 5) {
+                        updateStudyForm(studentToUpdate);
+                    } else if (option == 6) {
+                        updateStudentStatus(studentToUpdate);
+                    } else if (option == 7) {
+                        String email = inputValidator.readEmail("Enter a new email address for the student");
+                        studentToUpdate.setEmail(email);
+                    } else if (option == 8) {
+                        System.out.println("Enter a new phone number for the student: ");
+                        String phone = scan.nextLine();
+                        studentToUpdate.setPhoneNumber(phone);
+                    } else {
+                        break;
+                    }
+                    System.out.println("Student with an id = " + studentToUpdate.getId() + " was successfully updated!");
+                }
+            } else {
+                System.out.println("Student not found!");
+            }
+
+            if (!isRunning) break;
+
+            System.out.println("Would you like to update another one ? (yes - 1, no - 0): ");
+            int ans = scan.nextInt();
+            scan.nextLine();
+            while (ans != 1 && ans != 0) {
+                System.out.println("Invalid input! Try again: ");
+                ans = scan.nextInt();
+                scan.nextLine();
+            }
+            if (ans == 0) break;
+        }
+    }
+
+    private void updateStudentStatus(Student s) {
+        System.out.println("Choose new status");
+        StudentStatus[] statuses = StudentStatus.values();
+        for (int i = 0; i < statuses.length; i++) {
+            System.out.println(i + " - " + statuses[i]);
+        }
+
+        System.out.print("Your choice: ");
+        int choice = scan.nextInt();
+        scan.nextLine();
+
+        if (choice >= 0 && choice < statuses.length) {
+            s.setStatus(statuses[choice]);
+            System.out.println("Status updated");
+        } else {
+            System.out.println("Incorrect choice");
+        }
+    }
+
+    private void updateStudyForm(Student s) {
+        System.out.println("Choose new study form");
+        StudyForm[] forms = StudyForm.values();
+        for (int i = 0; i < forms.length; i++) {
+            System.out.println(i + " - " + forms[i]);
+        }
+        System.out.print("Your choice: ");
+        int choice = scan.nextInt();
+        scan.nextLine();
+
+        if (choice >= 0 && choice < forms.length) {
+            s.setStudyForm(forms[choice]);
+            System.out.println("Study form is updated");
+        } else {
+            System.out.println("Incorrect choice");
+        }
+    }
 }
