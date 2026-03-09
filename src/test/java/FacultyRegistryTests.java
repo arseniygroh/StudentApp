2d73b3d0c5cf1e76d9db77b677811255862ee63a
Arsenii Hrokh <a.m.groh07@gmail.com>
2026-03-09 20:22:59 +0200

    write tests for faculty and student registries

---
+++ src/test/java/FacultyRegistryTests.java
@@ -0,0 +1,69 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ukma.model.Faculty;
+import ukma.model.Teacher;
+import ukma.model.enums.Degree;
+import ukma.model.exception.FacultyNotFoundException;
+import ukma.model.utils.RegistryManager;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FacultyRegistryTests {
+    private RegistryManager manager;
+    private Teacher testDean;
+    private Faculty testFaculty;
+
+    @BeforeEach
+    public void init() {
+        manager = new RegistryManager();
+
+        testDean = new Teacher(
+                "Олександр", "Мельник", "Олександрович",
+                LocalDate.of(1980, 1, 1),
+                "dean@ukma.edu.ua", "0441112233",
+                Degree.PHD, "Доцент", "Доцент",
+                LocalDate.of(2010, 9, 1), 1.0
+        );
+        manager.addTeacher(testDean);
+        testFaculty = new Faculty("Факультет Інформатики", "ФІ", testDean, "fi_office@ukma.edu.ua", "0449998877");
+        manager.addFaculty(testFaculty);
+    }
+
+    @Test
+    public void getFacultyTest() {
+        int expectedId = testFaculty.getId();
+        Faculty found = manager.getFacultyById(expectedId);
+
+        assertNotNull(found);
+        assertEquals("Факультет Інформатики", found.getName());
+        assertEquals("ФІ", found.getShortName());
+    }
+
+    @Test
+    public void getNonExistingFacultyTest() {
+        assertThrows(FacultyNotFoundException.class, () -> {
+            manager.getFacultyById(10);
+        });
+    }
+
+    @Test
+    public void testAvailableTeachersForDean() {
+        Teacher freeTeacher = new Teacher(
+                "Вільний", "Викладач", "Петрович",
+                LocalDate.of(1985, 2, 2),
+                "free@ukma.edu.ua", "0990001122",
+                Degree.MASTER, "Асистент", "Асистент",
+                LocalDate.of(2020, 9, 1), 1.0
+        );
+        manager.addTeacher(freeTeacher);
+        List<Teacher> freeTeachers = manager.getAvailableTeachersForDean();
+
+        assertEquals(1, freeTeachers.size());
+        assertEquals(freeTeacher.getId(), freeTeachers.getFirst().getId());
+        assertTrue(!freeTeachers.contains(testDean));
+    }
+
+}
---
+++ src/test/java/StudentRegistryTests.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ukma.model.Faculty;
+import ukma.model.Student;
+import ukma.model.enums.StudentStatus;
+import ukma.model.enums.StudyForm;
+import ukma.model.exception.StudentNotFoundException;
+import ukma.model.utils.RegistryManager;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentRegistryTests {
+    private RegistryManager manager;
+    private Student testStudent;
+    private Faculty testFaculty;
+    @BeforeEach
+    public void initStudent() {
+        manager = new RegistryManager();
+        testFaculty = new Faculty("Факультет Інформатики", "ФІ", null, "fi@ukma.edu.ua", "0441234567");
+        manager.addFaculty(testFaculty);
+
+        testStudent = new Student(
+                "Ім'я", "Прізвище", "Побатькові",
+                LocalDate.of(2005, 5, 20),
+                "name@ukma.edu.ua", "0991234567",
+                "K-12345", 2, "121", 2023,
+                StudyForm.FULL_TIME, StudentStatus.BUDGET, testFaculty
+        );
+        manager.storeEmail(testStudent.getEmail());
+        manager.addStudent(testStudent);
+    }
+
+    @Test
+    public void getStudentTest() {
+        Student found = manager.getStudentById(testStudent.getId());
+        assertNotNull(found);
+        assertEquals("Ім'я", found.getFirstName());
+    }
+
+    @Test
+    public void getNonExistentStudentTest() {
+        assertThrows(StudentNotFoundException.class, () -> {
+            manager.getStudentById(10);
+        });
+    }
+
+    @Test
+    public void addStudentTest() {
+        Student studentToAdd = new Student("name", "surname", "fathersname",
+                LocalDate.of(2005, 5, 20),
+                "name2@ukma.edu.ua", "232738274872",
+                "K-23214", 3, "F-3", 2024,
+                StudyForm.FULL_TIME, StudentStatus.CONTRACT, testFaculty);
+        manager.addStudent(studentToAdd);
+
+        assertEquals(studentToAdd, manager.getStudentById(studentToAdd.getId()));
+    }
+
+    @Test
+    public void deleteStudentTest() {
+        long expectedId = testStudent.getId();
+        manager.deleteStudent(expectedId);
+
+        assertThrows(StudentNotFoundException.class, () -> {
+            manager.getStudentById(expectedId);
+        });
+    }
+
+    @Test
+    public void addStudentWithDuplicateEmailTest() {
+        Student cloneEmailStudent = new Student(
+                "Інше", "прізвище", "Побатькові",
+                LocalDate.of(2005, 5, 20),
+                "name@ukma.edu.ua", "0990000000",
+                "K-99999", 1, "121", 2024,
+                StudyForm.FULL_TIME, StudentStatus.BUDGET, testFaculty
+        );
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            manager.storeEmail(cloneEmailStudent.getEmail());
+        });
+
+        assertTrue(exception.getMessage().contains("already exists"));
+    }
+
+}
