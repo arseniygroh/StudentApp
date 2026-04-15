37f3ce3724cf0d31c64317c674cd3b9b04ed1c75
Piatachenko Hlib <h.piatachenko@ukma.edu.ua>
2026-04-16 00:00:46 +0300

    feat: add annotation length to university

--- src/main/java/ukma/domain/University.java
+++ src/main/java/ukma/domain/University.java
@@ -1,10 +1,20 @@
 package ukma.domain;
 
-public record University(String name, String shortName, String city, String address) {
+import ukma.domain.annotations.Length;
+import ukma.service.validation.AnnotationValidator;
+
+public record University(
+        @Length(min = 5, max = 100) String name,
+        @Length(min = 2, max = 10) String shortName,
+        @Length(min = 2, max = 50) String city,
+        @Length(min = 5, max = 100) String address
+) {
     public University {
         if (name == null || name.isBlank()) throw new IllegalArgumentException("Name can't be empty");
         if (shortName == null || shortName.isBlank()) throw new IllegalArgumentException("Short name can't be empty");
         if (city == null || city.isBlank()) throw new IllegalArgumentException("City can't be empty");
         if (address == null || address.isBlank()) throw new IllegalArgumentException("Address can't be empty");
+
+        AnnotationValidator.validate(this);
     }
 }
